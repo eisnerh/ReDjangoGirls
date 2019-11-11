e265af5b1730bbbd00ff59fca1fbecc4a831da93
eisnerh <elopeza@ucatolica.ac.cr>
2019-11-11 17:43:41 -0600

    Second Commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def postlist(request):
-    return render(request, 'blog/postlist.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/postlist.html', {'posts':posts})
